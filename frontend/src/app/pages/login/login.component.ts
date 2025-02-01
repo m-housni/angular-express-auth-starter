ec47fa89be45f2d2171e483381113314358bc08e
m-housni <mohamedhousni1000k@gmail.com>
2025-02-01 14:44:20 +0100

    Refactor login component to use dependency injection and improve error handling

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+// ==========================
+// # Imports
+// ==========================
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +12,27 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  // ==========================
+  // Dependency Injections
+  // ==========================
+  router = inject(Router);
+  authService = inject(AuthService);
+
+  // ==========================
+  // Initializations
+  // ==========================
+  error: string = '';
+
+
+  // ==========================
+  // Methods
+  // ==========================
+  login(email: string, password: string) {
+    this.authService.login(email, password).subscribe({
+      next: () => this.router.navigate(['/dashboard']),
+      error: (err) => this.error = 'Invalid credentials'
+    });
+  }
 
 }
